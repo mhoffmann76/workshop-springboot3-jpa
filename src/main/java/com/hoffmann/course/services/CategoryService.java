854513aae3ed93375aa689762c376b508f0b2093
mhoffmann76 <hoffmannmarcelo@hotmail.com>
2024-01-28 21:27:04 -0300

    Category

---
+++ src/main/java/com/hoffmann/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.hoffmann.course.services;
+
+import com.hoffmann.course.entities.Category;
+import com.hoffmann.course.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll(){
+
+        return repository.findAll();
+    }
+
+    public Category findById(Long id){
+        Optional<Category> obj =  repository.findById(id);
+        return obj.get();
+    }
+}
--- src/main/java/com/hoffmann/course/services/UserService.java
+++ src/main/java/com/hoffmann/course/services/UserService.java
@@ -1,10 +1,8 @@
 package com.hoffmann.course.services;
-
 import com.hoffmann.course.entities.User;
 import com.hoffmann.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 
